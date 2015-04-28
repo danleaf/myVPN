cb374b43f919f01a23024b6c12e9d67478f2cd83
danleaf <danleaf@live.cn>
2015-04-28 14:16:35 +0800

    11001

---
+++ voice/main.cpp
@@ -0,0 +1,344 @@
+#include <stdio.h>
+
+#define EXPR 0x04C11DB7
+#define EXPRH8 4
+#define LSB
+unsigned int map[256];
+
+struct Reg32
+{
+	union {
+		unsigned int r;
+		struct
+		{
+#ifndef LSB
+			unsigned int b0 : 1;
+			unsigned int b1 : 1;
+			unsigned int b2 : 1;
+			unsigned int b3 : 1;
+			unsigned int b4 : 1;
+			unsigned int b5 : 1;
+			unsigned int b6 : 1;
+			unsigned int b7 : 1;
+
+			unsigned int b8 : 1;
+			unsigned int b9 : 1;
+			unsigned int b10 : 1;
+			unsigned int b11 : 1;
+			unsigned int b12 : 1;
+			unsigned int b13 : 1;
+			unsigned int b14 : 1;
+			unsigned int b15 : 1;
+
+			unsigned int b16 : 1;
+			unsigned int b17 : 1;
+			unsigned int b18 : 1;
+			unsigned int b19 : 1;
+			unsigned int b20 : 1;
+			unsigned int b21 : 1;
+			unsigned int b22 : 1;
+			unsigned int b23 : 1;
+
+			unsigned int b24 : 1;
+			unsigned int b25 : 1;
+			unsigned int b26 : 1;
+			unsigned int b27 : 1;
+			unsigned int b28 : 1;
+			unsigned int b29 : 1;
+			unsigned int b30 : 1;
+			unsigned int b31 : 1;
+#else
+			unsigned int b31 : 1;
+			unsigned int b30 : 1;
+			unsigned int b29 : 1;
+			unsigned int b28 : 1;
+			unsigned int b27 : 1;
+			unsigned int b26 : 1;
+			unsigned int b25 : 1;
+			unsigned int b24 : 1;
+
+			unsigned int b23 : 1;
+			unsigned int b22 : 1;
+			unsigned int b21 : 1;
+			unsigned int b20 : 1;
+			unsigned int b19 : 1;
+			unsigned int b18 : 1;
+			unsigned int b17 : 1;
+			unsigned int b16 : 1;
+
+			unsigned int b15 : 1;
+			unsigned int b14 : 1;
+			unsigned int b13 : 1;
+			unsigned int b12 : 1;
+			unsigned int b11 : 1;
+			unsigned int b10 : 1;
+			unsigned int b9 : 1;
+			unsigned int b8 : 1;
+
+			unsigned int b7 : 1;
+			unsigned int b6 : 1;
+			unsigned int b5 : 1;
+			unsigned int b4 : 1;
+			unsigned int b3 : 1;
+			unsigned int b2 : 1;
+			unsigned int b1 : 1;
+			unsigned int b0 : 1;
+#endif
+		}bits;
+	}un;
+
+	Reg32(unsigned int n)
+	{
+		un.r = n;
+	}
+
+	Reg32()
+	{
+
+	}
+};
+
+struct Reg8
+{
+	union{
+		unsigned char r;
+		struct
+		{
+			unsigned int b0 : 1;
+			unsigned int b1 : 1;
+			unsigned int b2 : 1;
+			unsigned int b3 : 1;
+			unsigned int b4 : 1;
+			unsigned int b5 : 1;
+			unsigned int b6 : 1;
+			unsigned int b7 : 1;
+		}bits;
+		struct
+		{
+#ifndef LSB
+			unsigned int d7 : 1;
+			unsigned int d6 : 1;
+			unsigned int d5 : 1;
+			unsigned int d4 : 1;
+			unsigned int d3 : 1;
+			unsigned int d2 : 1;
+			unsigned int d1 : 1;
+			unsigned int d0 : 1;
+#else
+			unsigned int d0 : 1;
+			unsigned int d1 : 1;
+			unsigned int d2 : 1;
+			unsigned int d3 : 1;
+			unsigned int d4 : 1;
+			unsigned int d5 : 1;
+			unsigned int d6 : 1;
+			unsigned int d7 : 1;
+#endif
+		}tm;
+	}un;
+
+	Reg8(unsigned char n)
+	{
+		un.r = n;
+	}
+
+	Reg8()
+	{
+
+	}
+};
+
+unsigned int computmap(unsigned char head)
+{
+	unsigned int add[8];
+
+	for (int i = 7; i >= 0; i--)
+	{
+		if (128 & head)
+		{
+			add[i] = EXPR << i;
+			head <<= 1;
+			head ^= EXPRH8;
+		}
+		else
+		{
+			add[i] = 0;
+			head <<= 1;
+		}
+	}
+	
+	unsigned int sum = 0;
+
+	for (int i = 7; i >= 0; i--)
+	{
+		sum ^= add[i];
+	}
+	
+	return sum;
+}
+
+unsigned int comput8(unsigned int r, unsigned char d)
+{
+	r ^= d << 24;
+	unsigned char index = (r & 0xFF000000) >> 24;
+
+	unsigned int add = map[index];
+
+	r <<= 8;
+
+	return r^add;
+}
+
+unsigned int comput1(unsigned int r, unsigned char d)
+{
+	r ^= (d & 1) << 31;
+	unsigned char index = (r & 0x80000000) >> 31;
+
+	unsigned int add = map[index];
+
+	r <<= 1;
+
+	return r^add;
+}
+
+unsigned int comput1p(unsigned int r, unsigned char d)
+{
+	Reg32 ro(r);
+	Reg32 rn;
+	Reg8 sig;
+	sig.un.bits.b0 = (d & 1) ^ ro.un.bits.b31;
+
+	rn.un.bits.b31 = ro.un.bits.b30;
+	rn.un.bits.b30 = ro.un.bits.b29;
+	rn.un.bits.b29 = ro.un.bits.b28;
+	rn.un.bits.b28 = ro.un.bits.b27;
+	rn.un.bits.b27 = ro.un.bits.b26;
+	rn.un.bits.b26 = ro.un.bits.b25 ^ sig.un.bits.b0;
+	rn.un.bits.b25 = ro.un.bits.b24;
+	rn.un.bits.b24 = ro.un.bits.b23;
+	rn.un.bits.b23 = ro.un.bits.b22 ^ sig.un.bits.b0;
+	rn.un.bits.b22 = ro.un.bits.b21 ^ sig.un.bits.b0;
+	rn.un.bits.b21 = ro.un.bits.b20;
+	rn.un.bits.b20 = ro.un.bits.b19;
+	rn.un.bits.b19 = ro.un.bits.b18;
+	rn.un.bits.b18 = ro.un.bits.b17;
+	rn.un.bits.b17 = ro.un.bits.b16;
+	rn.un.bits.b16 = ro.un.bits.b15 ^ sig.un.bits.b0;
+	rn.un.bits.b15 = ro.un.bits.b14;
+	rn.un.bits.b14 = ro.un.bits.b13;
+	rn.un.bits.b13 = ro.un.bits.b12;
+	rn.un.bits.b12 = ro.un.bits.b11 ^ sig.un.bits.b0;
+	rn.un.bits.b11 = ro.un.bits.b10 ^ sig.un.bits.b0;
+	rn.un.bits.b10 = ro.un.bits.b9 ^ sig.un.bits.b0;
+	rn.un.bits.b9 = ro.un.bits.b8;
+	rn.un.bits.b8 = ro.un.bits.b7 ^ sig.un.bits.b0;
+	rn.un.bits.b7 = ro.un.bits.b6 ^ sig.un.bits.b0;
+	rn.un.bits.b6 = ro.un.bits.b5;
+	rn.un.bits.b5 = ro.un.bits.b4 ^ sig.un.bits.b0;
+	rn.un.bits.b4 = ro.un.bits.b3 ^ sig.un.bits.b0;
+	rn.un.bits.b3 = ro.un.bits.b2;
+	rn.un.bits.b2 = ro.un.bits.b1 ^ sig.un.bits.b0;
+	rn.un.bits.b1 = ro.un.bits.b0 ^ sig.un.bits.b0;
+	rn.un.bits.b0 = sig.un.bits.b0;
+
+	return rn.un.r;
+}
+
+unsigned int comput8p(unsigned int r, unsigned char d)
+{
+	Reg32 ro(r);
+	Reg32 rn;
+	Reg8 dd(d);
+
+	rn.un.bits.b31 = ro.un.bits.b23 ^ ro.un.bits.b29 ^ dd.un.tm.d2;
+	rn.un.bits.b30 = ro.un.bits.b22 ^ ro.un.bits.b28 ^ ro.un.bits.b31 ^ dd.un.tm.d0 ^ dd.un.tm.d3;
+	rn.un.bits.b29 = ro.un.bits.b21 ^ ro.un.bits.b27 ^ ro.un.bits.b30 ^ ro.un.bits.b31 ^ dd.un.tm.d0 ^ dd.un.tm.d1 ^ dd.un.tm.d4;
+	rn.un.bits.b28 = ro.un.bits.b20 ^ ro.un.bits.b30 ^ ro.un.bits.b29 ^ ro.un.bits.b26 ^ dd.un.tm.d1 ^ dd.un.tm.d2 ^ dd.un.tm.d5;
+	rn.un.bits.b27 = ro.un.bits.b19 ^ ro.un.bits.b25 ^ ro.un.bits.b28 ^ ro.un.bits.b29 ^ ro.un.bits.b31 ^ dd.un.tm.d0 ^ dd.un.tm.d2 ^ dd.un.tm.d3 ^ dd.un.tm.d6;
+	rn.un.bits.b26 = ro.un.bits.b18 ^ ro.un.bits.b24 ^ ro.un.bits.b27 ^ ro.un.bits.b28 ^ ro.un.bits.b30 ^ dd.un.tm.d1 ^ dd.un.tm.d3 ^ dd.un.tm.d4 ^ dd.un.tm.d7;
+	rn.un.bits.b25 = ro.un.bits.b17 ^ ro.un.bits.b26 ^ ro.un.bits.b27 ^ dd.un.tm.d4 ^ dd.un.tm.d5;
+	rn.un.bits.b24 = ro.un.bits.b16 ^ ro.un.bits.b25 ^ ro.un.bits.b26 ^ ro.un.bits.b31 ^ dd.un.tm.d0 ^ dd.un.tm.d5 ^ dd.un.tm.d6;
+	
+	rn.un.bits.b23 = ro.un.bits.b15 ^ ro.un.bits.b24 ^ ro.un.bits.b25 ^ ro.un.bits.b30 ^ dd.un.tm.d1 ^ dd.un.tm.d6 ^ dd.un.tm.d7;
+	rn.un.bits.b22 = ro.un.bits.b14 ^ ro.un.bits.b24 ^ dd.un.tm.d7;
+	rn.un.bits.b21 = ro.un.bits.b13 ^ ro.un.bits.b29 ^ dd.un.tm.d2;
+	rn.un.bits.b20 = ro.un.bits.b12 ^ ro.un.bits.b28 ^ dd.un.tm.d3;
+	rn.un.bits.b19 = ro.un.bits.b11 ^ ro.un.bits.b27 ^ ro.un.bits.b31 ^ dd.un.tm.d0 ^ dd.un.tm.d4;
+	rn.un.bits.b18 = ro.un.bits.b10 ^ ro.un.bits.b26 ^ ro.un.bits.b30 ^ ro.un.bits.b31 ^ dd.un.tm.d0 ^ dd.un.tm.d1 ^ dd.un.tm.d5;
+	rn.un.bits.b17 = ro.un.bits.b9 ^ ro.un.bits.b25 ^ ro.un.bits.b29 ^ ro.un.bits.b30 ^ dd.un.tm.d1 ^ dd.un.tm.d2 ^ dd.un.tm.d6;
+	rn.un.bits.b16 = ro.un.bits.b8 ^ ro.un.bits.b24 ^ ro.un.bits.b28 ^ ro.un.bits.b29 ^ dd.un.tm.d2 ^ dd.un.tm.d3 ^ dd.un.tm.d7;
+	
+	rn.un.bits.b15 = ro.un.bits.b7 + ro.un.bits.b27 + ro.un.bits.b28 + ro.un.bits.b29 + ro.un.bits.b31 + dd.un.tm.d0 + dd.un.tm.d2 + dd.un.tm.d3 + dd.un.tm.d4;
+	rn.un.bits.b14 = ro.un.bits.b6 + ro.un.bits.b26 + ro.un.bits.b27 + ro.un.bits.b28 + ro.un.bits.b30 + ro.un.bits.b31 + dd.un.tm.d0 + dd.un.tm.d1 + dd.un.tm.d3 + dd.un.tm.d4 + dd.un.tm.d5;
+	rn.un.bits.b13 = ro.un.bits.b5 + ro.un.bits.b25 + ro.un.bits.b26 + ro.un.bits.b27 + ro.un.bits.b29 + ro.un.bits.b30 + ro.un.bits.b31 + dd.un.tm.d0 + dd.un.tm.d1 + dd.un.tm.d2 + dd.un.tm.d4 + dd.un.tm.d5 + dd.un.tm.d6;
+	rn.un.bits.b12 = ro.un.bits.b4 + ro.un.bits.b24 + ro.un.bits.b25 + ro.un.bits.b26 + ro.un.bits.b28 + ro.un.bits.b29 + ro.un.bits.b30 + dd.un.tm.d1 + dd.un.tm.d2 + dd.un.tm.d3 + dd.un.tm.d5 + dd.un.tm.d6 + dd.un.tm.d7;
+	rn.un.bits.b11 = ro.un.bits.b3 + ro.un.bits.b24 + ro.un.bits.b25 + ro.un.bits.b27 + ro.un.bits.b28 + dd.un.tm.d3 + dd.un.tm.d4 + dd.un.tm.d6 + dd.un.tm.d7;
+	rn.un.bits.b10 = ro.un.bits.b2 + ro.un.bits.b24 + ro.un.bits.b26 + ro.un.bits.b27 + ro.un.bits.b29 + dd.un.tm.d2 + dd.un.tm.d4 + dd.un.tm.d5 + dd.un.tm.d7;
+	rn.un.bits.b9 = ro.un.bits.b1 + ro.un.bits.b25 + ro.un.bits.b26 + ro.un.bits.b28 + ro.un.bits.b29 + dd.un.tm.d2 + dd.un.tm.d3 + dd.un.tm.d5 + dd.un.tm.d6;
+	rn.un.bits.b8 = ro.un.bits.b0 + ro.un.bits.b24 + ro.un.bits.b25 + ro.un.bits.b27 + ro.un.bits.b28 + dd.un.tm.d3 + dd.un.tm.d4 + dd.un.tm.d6 + dd.un.tm.d7;
+	
+	rn.un.bits.b7 = ro.un.bits.b24 + ro.un.bits.b26 + ro.un.bits.b27 + ro.un.bits.b29 + ro.un.bits.b31 + dd.un.tm.d0 + dd.un.tm.d2 + dd.un.tm.d4 + dd.un.tm.d5 + dd.un.tm.d7;
+	rn.un.bits.b6 = ro.un.bits.b25 + ro.un.bits.b26 + ro.un.bits.b28 + ro.un.bits.b29 + ro.un.bits.b30 + ro.un.bits.b31 + dd.un.tm.d0 + dd.un.tm.d1 + dd.un.tm.d2 + dd.un.tm.d3 + dd.un.tm.d5 + dd.un.tm.d6;
+	rn.un.bits.b5 = ro.un.bits.b24 + ro.un.bits.b25 + ro.un.bits.b27 + ro.un.bits.b28 + ro.un.bits.b29 + ro.un.bits.b30 + ro.un.bits.b31 + dd.un.tm.d0 + dd.un.tm.d1 + dd.un.tm.d2 + dd.un.tm.d3 + dd.un.tm.d4 + dd.un.tm.d6 + dd.un.tm.d7;
+	rn.un.bits.b4 = ro.un.bits.b24 + ro.un.bits.b26 + ro.un.bits.b27 + ro.un.bits.b28 + ro.un.bits.b30 + dd.un.tm.d1 + dd.un.tm.d3 + dd.un.tm.d4 + dd.un.tm.d5 + dd.un.tm.d7;
+	rn.un.bits.b3 = ro.un.bits.b25 + ro.un.bits.b26 + ro.un.bits.b27 + ro.un.bits.b31 + dd.un.tm.d0 + dd.un.tm.d4 + dd.un.tm.d5 + dd.un.tm.d6;
+	rn.un.bits.b2 = ro.un.bits.b24 + ro.un.bits.b25 + ro.un.bits.b26 + ro.un.bits.b30 + ro.un.bits.b31 + dd.un.tm.d0 + dd.un.tm.d1 + dd.un.tm.d5 + dd.un.tm.d6 + dd.un.tm.d7;
+	rn.un.bits.b1 = ro.un.bits.b24 + ro.un.bits.b25 + ro.un.bits.b30 + ro.un.bits.b31 + dd.un.tm.d0 + dd.un.tm.d1 + dd.un.tm.d6 + dd.un.tm.d7;
+	rn.un.bits.b0 = ro.un.bits.b24 + ro.un.bits.b30 + dd.un.tm.d1 + dd.un.tm.d7;
+
+	return rn.un.r;
+}
+
+int main()
+{
+	int a;
+
+	for (int i = 0; i < 256; i++)
+	{
+		map[i] = computmap((unsigned char)i);
+	}
+
+
+	unsigned long long x;
+	unsigned int r = 0;
+
+	x = 0xAA9CAAAA05FF0000;
+	r = 0;
+	for (int i = 0; i < 64; i++)
+	{
+		unsigned char d = (x & 0x8000000000000000) >> 63;
+		r = comput1(r, d);
+		x <<= 1;
+	}
+
+	printf("%X\n", r);
+
+
+
+	x = 0xAA9CAAAA05FF0000;
+	r = 0;
+	for (int i = 0; i < 64; i++)
+	{
+		unsigned char d = (x & 0x8000000000000000) >> 63;
+		r = comput1p(r, d);
+		x <<= 1;
+	}
+
+	printf("%X\n", r);
+
+
+	x = 0xAA9CAAAA05FF0000;
+	r = 0;
+	for (int i = 0; i < 8; i++)
+	{
+		unsigned char d = (x & 0xFF00000000000000) >> 56;
+		r = comput8p(r, d);
+		x <<= 8;
+	}
+
+	printf("%X\n", r);
+
+	scanf_s("%d", &a);
+
+	return 0;
+}
---
+++ voice/main2.cpp
@@ -0,0 +1,364 @@
+#include <stdio.h>
+#include <list>
+#include <map>
+#include <iostream>
+using namespace std;
+
+struct P
+{
+	int g;
+	int idx;
+
+	P()
+	{
+		g = -1;
+		idx = -1;
+	}
+
+	P(int ig, int iidx)
+	{
+		g = ig;
+		idx = iidx;
+	}
+
+	bool operator <(const P& other) const
+	{
+		if (idx < 100 || other.idx < 100)
+			return idx < other.idx;
+
+		if (idx != other.idx)
+			return idx < other.idx;
+
+		return g < other.g;
+	}
+
+	bool operator <(const P& other)
+	{
+		return idx < other.idx;
+	}
+
+	void Print()
+	{
+		if (idx < 100)
+			cout << "R" << idx << "_" << g;
+		else
+			cout << "d" << g;
+	}
+};
+
+struct Assign
+{
+	P left;
+	list<P> right;
+
+	Assign()
+	{
+
+	}
+
+	Assign(const P& pleft, const P& pright)
+	{
+		left = pleft;
+		right.push_back(pright);
+	}
+
+	Assign(const P& pleft, const P& pright1, const P& pright2)
+	{
+		left = pleft;
+		right.push_back(pright1);
+		right.push_back(pright2);
+	}
+
+	Assign(const P& pleft, const P& pright1, const P& pright2, const P& pright3)
+	{
+		left = pleft;
+		right.push_back(pright1);
+		right.push_back(pright2);
+		right.push_back(pright3);
+	}
+
+	void Print()
+	{
+		left.Print();
+		cout << " = ";
+
+		int c = 0;
+		for (list<P>::iterator it = right.begin();
+			it != right.end(); ++it)
+		{
+			c++;
+			it->Print();
+			if (c < right.size())
+				cout << " + ";
+			else
+				cout << endl;
+		}
+	}
+
+	void Unit()
+	{
+		map<P, int> m;
+		for (list<P>::iterator it = right.begin();
+			it != right.end(); ++it)
+		{
+			if (m.find(*it) == m.end())
+			{
+				m[*it] = 1;
+			}
+			else
+			{
+				m[*it]++;
+			}
+		}
+
+		right.clear();
+		for (map<P, int>::iterator it = m.begin();
+			it != m.end(); ++it)
+		{
+			if (it->second % 2 == 1)
+				right.push_back(it->first);
+		}
+	}
+};
+
+map<P, Assign> pmap;
+
+bool replace(const P& p, list<P>& subg)
+{
+	subg.clear();
+	if (0 == p.g)
+		return false;
+
+	map<P, Assign>::iterator itFind = pmap.find(p);
+	if (itFind == pmap.end())
+		return false;
+
+	Assign& assign = itFind->second;
+
+	for (list<P>::iterator it = assign.right.begin();
+		it != assign.right.end(); ++it)
+	{
+		P tmp = *it;
+		list<P> tmpList;
+
+		tmp.g = p.g - 1;
+
+		bool ret = replace(tmp, tmpList);
+		if (!ret)
+		{
+			subg.push_back(tmp);
+		}
+		else
+		{
+			for (list<P>::iterator it2 = tmpList.begin();
+				it2 != tmpList.end(); ++it2)
+			{
+				subg.push_back(*it2);
+			}
+		}
+	}
+
+	return true;
+}
+
+int main0()
+{
+	pmap[P(1, 31)] = Assign(P(1, 31), P(0, 30));
+	pmap[P(1, 30)] = Assign(P(1, 30), P(0, 29));
+	pmap[P(1, 29)] = Assign(P(1, 29), P(0, 28));
+	pmap[P(1, 28)] = Assign(P(1, 28), P(0, 27));
+	pmap[P(1, 27)] = Assign(P(1, 27), P(0, 26));
+	pmap[P(1, 26)] = Assign(P(1, 26), P(0, 25), P(0, 31), P(0, 100));
+	pmap[P(1, 25)] = Assign(P(1, 25), P(0, 24));
+	pmap[P(1, 24)] = Assign(P(1, 24), P(0, 23));
+	pmap[P(1, 23)] = Assign(P(1, 23), P(0, 22), P(0, 31), P(0, 100));
+	pmap[P(1, 22)] = Assign(P(1, 22), P(0, 21), P(0, 31), P(0, 100));
+	pmap[P(1, 21)] = Assign(P(1, 21), P(0, 20));
+	pmap[P(1, 20)] = Assign(P(1, 20), P(0, 19));
+	pmap[P(1, 19)] = Assign(P(1, 19), P(0, 18));
+	pmap[P(1, 18)] = Assign(P(1, 18), P(0, 17));
+	pmap[P(1, 17)] = Assign(P(1, 17), P(0, 16));
+	pmap[P(1, 16)] = Assign(P(1, 16), P(0, 15), P(0, 31), P(0, 100));
+	pmap[P(1, 15)] = Assign(P(1, 15), P(0, 14));
+	pmap[P(1, 14)] = Assign(P(1, 14), P(0, 13));
+	pmap[P(1, 13)] = Assign(P(1, 13), P(0, 12));
+	pmap[P(1, 12)] = Assign(P(1, 12), P(0, 11), P(0, 31), P(0, 100));
+	pmap[P(1, 11)] = Assign(P(1, 11), P(0, 10), P(0, 31), P(0, 100));
+	pmap[P(1, 10)] = Assign(P(1, 10), P(0, 9), P(0, 31), P(0, 100));
+	pmap[P(1, 9)] = Assign(P(1, 9), P(0, 8));
+	pmap[P(1, 8)] = Assign(P(1, 8), P(0, 7), P(0, 31), P(0, 100));
+	pmap[P(1, 7)] = Assign(P(1, 7), P(0, 6), P(0, 31), P(0, 100));
+	pmap[P(1, 6)] = Assign(P(1, 6), P(0, 5));
+	pmap[P(1, 5)] = Assign(P(1, 5), P(0, 4), P(0, 31), P(0, 100));
+	pmap[P(1, 4)] = Assign(P(1, 4), P(0, 3), P(0, 31), P(0, 100));
+	pmap[P(1, 3)] = Assign(P(1, 3), P(0, 2));
+	pmap[P(1, 2)] = Assign(P(1, 2), P(0, 1), P(0, 31), P(0, 100));
+	pmap[P(1, 1)] = Assign(P(1, 1), P(0, 0), P(0, 31), P(0, 100));
+	pmap[P(1, 0)] = Assign(P(1, 0), P(0, 31), P(0, 100));
+
+	Assign assign;
+
+	assign.left = P(8, 31);
+	replace(assign.left, assign.right);
+	assign.Unit();
+	assign.Print();
+
+	assign.left = P(8, 30);
+	replace(assign.left, assign.right);
+	assign.Unit();
+	assign.Print();
+
+	assign.left = P(8, 29);
+	replace(assign.left, assign.right);
+	assign.Unit();
+	assign.Print();
+
+	assign.left = P(8, 28);
+	replace(assign.left, assign.right);
+	assign.Print();
+
+	assign.left = P(8, 27);
+	replace(assign.left, assign.right);
+	assign.Unit();
+	assign.Print();
+
+	assign.left = P(8, 26);
+	replace(assign.left, assign.right);
+	assign.Unit();
+	assign.Print();
+
+	assign.left = P(8, 25);
+	replace(assign.left, assign.right);
+	assign.Unit();
+	assign.Print();
+
+	assign.left = P(8, 24);
+	replace(assign.left, assign.right);
+	assign.Unit();
+	assign.Print();
+
+	assign.left = P(8, 23);
+	replace(assign.left, assign.right);
+	assign.Unit();
+	assign.Print();
+
+	assign.left = P(8, 22);
+	replace(assign.left, assign.right);
+	assign.Unit();
+	assign.Print();
+
+	assign.left = P(8, 21);
+	replace(assign.left, assign.right);
+	assign.Unit();
+	assign.Print();
+
+	assign.left = P(8, 20);
+	replace(assign.left, assign.right);
+	assign.Print();
+
+	assign.left = P(8, 19);
+	replace(assign.left, assign.right);
+	assign.Unit();
+	assign.Print();
+
+	assign.left = P(8, 18);
+	replace(assign.left, assign.right);
+	assign.Unit();
+	assign.Print();
+
+	assign.left = P(8, 17);
+	replace(assign.left, assign.right);
+	assign.Unit();
+	assign.Print();
+
+	assign.left = P(8, 16);
+	replace(assign.left, assign.right);
+	assign.Unit();
+	assign.Print();
+
+	assign.left = P(8, 15);
+	replace(assign.left, assign.right);
+	assign.Unit();
+	assign.Print();
+
+	assign.left = P(8, 14);
+	replace(assign.left, assign.right);
+	assign.Unit();
+	assign.Print();
+
+	assign.left = P(8, 13);
+	replace(assign.left, assign.right);
+	assign.Unit();
+	assign.Print();
+
+	assign.left = P(8, 12);
+	replace(assign.left, assign.right);
+	assign.Unit();
+	assign.Print();
+
+	assign.left = P(8, 11);
+	replace(assign.left, assign.right);
+	assign.Unit();
+	assign.Print();
+
+	assign.left = P(8, 10);
+	replace(assign.left, assign.right);
+	assign.Unit();
+	assign.Print();
+
+	assign.left = P(8, 9);
+	replace(assign.left, assign.right);
+	assign.Unit();
+	assign.Print();
+
+	assign.left = P(8, 8);
+	replace(assign.left, assign.right);
+	assign.Unit();
+	assign.Print();
+
+	assign.left = P(8, 7);
+	replace(assign.left, assign.right);
+	assign.Unit();
+	assign.Print();
+
+	assign.left = P(8, 6);
+	replace(assign.left, assign.right);
+	assign.Unit();
+	assign.Print();
+
+	assign.left = P(8, 5);
+	replace(assign.left, assign.right);
+	assign.Unit();
+	assign.Print();
+
+	assign.left = P(8, 4);
+	replace(assign.left, assign.right);
+	assign.Unit();
+	assign.Print();
+
+	assign.left = P(8, 3);
+	replace(assign.left, assign.right);
+	assign.Unit();
+	assign.Print();
+
+	assign.left = P(8, 2);
+	replace(assign.left, assign.right);
+	assign.Unit();
+	assign.Print();
+
+	assign.left = P(8, 1);
+	replace(assign.left, assign.right);
+	assign.Unit();
+	assign.Print();
+
+	assign.left = P(8, 0);
+	replace(assign.left, assign.right);
+	assign.Unit();
+	assign.Print();
+
+
+
+	int a;
+	scanf_s("%d", &a);
+	return 0;
+}
